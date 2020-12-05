f9bd889996b9dabc8c28d90007aa40cfeec17259
xcoru <pcd510@hotmail.com>
2020-12-05 12:46:37 -0600

    Update Principal.vb

--- OrdenamientoXML/OrdenamientoXML/Principal.vb
+++ OrdenamientoXML/OrdenamientoXML/Principal.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Principal
+
     Private Sub Principal_Load(sender As Object, e As EventArgs) Handles Me.Load
+        CheckForIllegalCrossThreadCalls = False
         InicializarVariables()
     End Sub
 
@@ -16,10 +18,16 @@
     Private Sub BtnIniciar_Click(sender As Object, e As EventArgs) Handles btnIniciar.Click
         If txtCarpeta.TextLength > 0 Then
             Folder = txtCarpeta.Text
+            btnIniciar.Enabled = False
+            btnProgreso.Visible = True
+            ProcesoActivo = True
+            TimerActualizacion.Enabled = True
+
+            Tipos.LimpiarTotales()
 
             InicializarVariables()
             IniciarBusqueda()
-            btnProgreso.Visible = True
+
         Else
             Msg("Selecciona una carpeta!", 2)
         End If
@@ -30,5 +38,56 @@
         txtTotalNuevos.Text = "0"
         txtTotalXML.Text = "0"
         dgvTabla.Rows.Clear()
+    End Sub
+
+    Public Sub ActualizarTabla()
+        Dim i As Integer
+        i = 0
+
+        Try
+            If dgvTabla.Rows.Count = 0 Then
+                For Each linea As I_Tipo In Tipos.Tipos
+                    dgvTabla.Rows.Add({i + 1, linea.Id, linea.Nombre, linea.Total})
+                    i += 1
+                Next
+            ElseIf dgvTabla.Rows.Count <= Tipos.Tipos.Count Then
+                For i = 0 To dgvTabla.Rows.Count - 1
+                    dgvTabla.Rows(i).Cells(3).Value = Tipos.Tipos(i).Total
+                Next
+            End If
+
+            If dgvTabla.Rows.Count < Tipos.Tipos.Count Then
+                For i = dgvTabla.Rows.Count To Tipos.Tipos.Count - 1
+                    dgvTabla.Rows.Add({i, Tipos.Tipos(i).Id, Tipos.Tipos(i).Nombre, Tipos.Tipos(i).Total})
+                Next
+            End If
+
+        Catch ex As Exception
+        End Try
+
+    End Sub
+
+    Public Sub ActualizarDatos()
+        Try
+            txtTotalXML.Text = N_TotalAnalizados
+            txtTotalNuevos.Text = N_TotalNuevos
+        Catch ex As Exception
+        End Try
+
+        ActualizarTabla()
+    End Sub
+
+    Private Sub TimerActualizacion_Tick(sender As Object, e As EventArgs) Handles TimerActualizacion.Tick
+
+        If ProcesoActivo Then
+            ActualizarDatos()
+        Else
+            ActualizarDatos()
+            btnProgreso.Visible = False
+            btnIniciar.Enabled = True
+
+            TimerActualizacion.Enabled = False
+        End If
+
     End Sub
 End Class
