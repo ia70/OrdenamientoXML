d80e02b1b3e69e8200f390734f4449b886e355cf
xcoru <pcd510@hotmail.com>
2020-12-05 12:46:31 -0600

    Update Principal.Designer.vb

--- OrdenamientoXML/OrdenamientoXML/Principal.Designer.vb
+++ OrdenamientoXML/OrdenamientoXML/Principal.Designer.vb
@@ -29,8 +29,15 @@ Partial Class Principal
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtCarpeta = New System.Windows.Forms.TextBox()
         Me.btnBuscar = New System.Windows.Forms.Button()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.dgvTabla = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.btnProgreso = New System.Windows.Forms.ProgressBar()
         Me.Label10 = New System.Windows.Forms.Label()
         Me.txtTotalNuevos = New System.Windows.Forms.Label()
@@ -38,14 +45,7 @@ Partial Class Principal
         Me.txtTotalXML = New System.Windows.Forms.Label()
         Me.btnIniciar = New System.Windows.Forms.Button()
         Me.dlgFolder = New System.Windows.Forms.FolderBrowserDialog()
-        Me.txtCarpeta = New System.Windows.Forms.TextBox()
-        Me.tmrActualizacion = New System.Windows.Forms.Timer(Me.components)
-        Me.dgvTabla = New System.Windows.Forms.DataGridView()
-        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.TimerActualizacion = New System.Windows.Forms.Timer(Me.components)
         Me.Panel1.SuspendLayout()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
@@ -95,6 +95,15 @@ Partial Class Principal
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Seleccionar carpeta:"
         '
+        'txtCarpeta
+        '
+        Me.txtCarpeta.Location = New System.Drawing.Point(9, 25)
+        Me.txtCarpeta.Multiline = True
+        Me.txtCarpeta.Name = "txtCarpeta"
+        Me.txtCarpeta.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtCarpeta.Size = New System.Drawing.Size(500, 47)
+        Me.txtCarpeta.TabIndex = 2
+        '
         'btnBuscar
         '
         Me.btnBuscar.BackColor = System.Drawing.Color.DodgerBlue
@@ -126,6 +135,75 @@ Partial Class Principal
         Me.GroupBox2.TabIndex = 2
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Detalles de análisis de XML"
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.BackColor = System.Drawing.Color.OrangeRed
+        Me.btnLimpiar.FlatAppearance.BorderColor = System.Drawing.Color.OrangeRed
+        Me.btnLimpiar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Coral
+        Me.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnLimpiar.ForeColor = System.Drawing.Color.White
+        Me.btnLimpiar.Location = New System.Drawing.Point(415, 224)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(170, 31)
+        Me.btnLimpiar.TabIndex = 20
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = False
+        '
+        'dgvTabla
+        '
+        Me.dgvTabla.AllowUserToAddRows = False
+        Me.dgvTabla.AllowUserToDeleteRows = False
+        Me.dgvTabla.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.dgvTabla.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.dgvTabla.BackgroundColor = System.Drawing.Color.White
+        Me.dgvTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvTabla.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column4, Me.Column2, Me.Column3})
+        Me.dgvTabla.Location = New System.Drawing.Point(8, 36)
+        Me.dgvTabla.Name = "dgvTabla"
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.White
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White
+        Me.dgvTabla.RowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.dgvTabla.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.White
+        Me.dgvTabla.RowTemplate.DefaultCellStyle.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dgvTabla.RowTemplate.DefaultCellStyle.ForeColor = System.Drawing.Color.Black
+        Me.dgvTabla.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        Me.dgvTabla.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.White
+        Me.dgvTabla.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.dgvTabla.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvTabla.Size = New System.Drawing.Size(385, 256)
+        Me.dgvTabla.TabIndex = 19
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "No."
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        Me.Column1.Width = 57
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Id"
+        Me.Column4.Name = "Column4"
+        Me.Column4.ReadOnly = True
+        Me.Column4.Width = 45
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Detalles"
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        Me.Column2.Width = 90
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "#"
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        Me.Column3.Width = 42
         '
         'btnProgreso
         '
@@ -200,87 +278,9 @@ Partial Class Principal
         '
         Me.dlgFolder.Description = "13 Ponientes Analistas"
         '
-        'txtCarpeta
+        'TimerActualizacion
         '
-        Me.txtCarpeta.Location = New System.Drawing.Point(9, 25)
-        Me.txtCarpeta.Multiline = True
-        Me.txtCarpeta.Name = "txtCarpeta"
-        Me.txtCarpeta.ScrollBars = System.Windows.Forms.ScrollBars.Both
-        Me.txtCarpeta.Size = New System.Drawing.Size(500, 47)
-        Me.txtCarpeta.TabIndex = 2
-        '
-        'tmrActualizacion
-        '
-        Me.tmrActualizacion.Interval = 500
-        '
-        'dgvTabla
-        '
-        Me.dgvTabla.AllowUserToAddRows = False
-        Me.dgvTabla.AllowUserToDeleteRows = False
-        Me.dgvTabla.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
-        Me.dgvTabla.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
-        Me.dgvTabla.BackgroundColor = System.Drawing.Color.White
-        Me.dgvTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvTabla.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column4, Me.Column2, Me.Column3})
-        Me.dgvTabla.Location = New System.Drawing.Point(8, 36)
-        Me.dgvTabla.Name = "dgvTabla"
-        DataGridViewCellStyle1.BackColor = System.Drawing.Color.White
-        DataGridViewCellStyle1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        DataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black
-        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
-        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White
-        Me.dgvTabla.RowsDefaultCellStyle = DataGridViewCellStyle1
-        Me.dgvTabla.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.White
-        Me.dgvTabla.RowTemplate.DefaultCellStyle.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.dgvTabla.RowTemplate.DefaultCellStyle.ForeColor = System.Drawing.Color.Black
-        Me.dgvTabla.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.SystemColors.Highlight
-        Me.dgvTabla.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.White
-        Me.dgvTabla.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.dgvTabla.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
-        Me.dgvTabla.Size = New System.Drawing.Size(385, 256)
-        Me.dgvTabla.TabIndex = 19
-        '
-        'Column1
-        '
-        Me.Column1.HeaderText = "No."
-        Me.Column1.Name = "Column1"
-        Me.Column1.ReadOnly = True
-        Me.Column1.Width = 57
-        '
-        'Column4
-        '
-        Me.Column4.HeaderText = "Id"
-        Me.Column4.Name = "Column4"
-        Me.Column4.ReadOnly = True
-        Me.Column4.Width = 45
-        '
-        'Column2
-        '
-        Me.Column2.HeaderText = "Detalles"
-        Me.Column2.Name = "Column2"
-        Me.Column2.ReadOnly = True
-        Me.Column2.Width = 90
-        '
-        'Column3
-        '
-        Me.Column3.HeaderText = "#"
-        Me.Column3.Name = "Column3"
-        Me.Column3.ReadOnly = True
-        Me.Column3.Width = 42
-        '
-        'btnLimpiar
-        '
-        Me.btnLimpiar.BackColor = System.Drawing.Color.OrangeRed
-        Me.btnLimpiar.FlatAppearance.BorderColor = System.Drawing.Color.OrangeRed
-        Me.btnLimpiar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Coral
-        Me.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnLimpiar.ForeColor = System.Drawing.Color.White
-        Me.btnLimpiar.Location = New System.Drawing.Point(415, 224)
-        Me.btnLimpiar.Name = "btnLimpiar"
-        Me.btnLimpiar.Size = New System.Drawing.Size(170, 31)
-        Me.btnLimpiar.TabIndex = 20
-        Me.btnLimpiar.Text = "Limpiar"
-        Me.btnLimpiar.UseVisualStyleBackColor = False
+        Me.TimerActualizacion.Interval = 500
         '
         'Principal
         '
@@ -323,11 +323,11 @@ Partial Class Principal
     Friend WithEvents btnProgreso As ProgressBar
     Friend WithEvents dlgFolder As FolderBrowserDialog
     Friend WithEvents txtCarpeta As TextBox
-    Friend WithEvents tmrActualizacion As Timer
     Friend WithEvents dgvTabla As DataGridView
     Friend WithEvents Column1 As DataGridViewTextBoxColumn
     Friend WithEvents Column4 As DataGridViewTextBoxColumn
     Friend WithEvents Column2 As DataGridViewTextBoxColumn
     Friend WithEvents Column3 As DataGridViewTextBoxColumn
     Friend WithEvents btnLimpiar As Button
+    Friend WithEvents TimerActualizacion As Timer
 End Class
